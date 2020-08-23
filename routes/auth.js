256fb0e335a955239ec9376f546f5d4e22011636
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-08-23 19:55:51 -0400

    created post delete and get routes in routes/auth file for login signup and logout

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const passport = require('../passport')
+const ctrl = require('../controllers')
+
+// PATH = /api/v1/auth
+router.post('/login', passport.authenticate('local'), ctrl.auth.login)
+router.post('/signUp', ctrl.auth.signUp)
+router.delete('/signOut', ctrl.auth.signOut)
+// utility route for devs
+router.get('/verify', ctrl.auth.verify)
+
+module.exports = router
